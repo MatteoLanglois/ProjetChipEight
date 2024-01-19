7b90af8988c40135f3d1409dc159744852075d00
matteolanglois <langlois.matteo49@gmail.com>
2024-01-19 10:54:43 +0100

    debug

--- chip8.c
+++ chip8.c
@@ -1,8 +1,8 @@
 #include "chip8.h"
 
 void chip8_init() {
-    if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_TIMER|SDL_INIT_AUDIO)) {
-            errcode = SDL;
+    if (SDL_Init(SDL_INIT_EVERYTHING)) {
+        printf("Error initializing SDL: %s\n", SDL_GetError());
     } else {
         struct chip8* chip8 = malloc(sizeof(struct chip8));
 
@@ -17,18 +17,25 @@ void chip8_init() {
         chip8->display = malloc(sizeof(struct Display));
         if (Display_init(chip8->display, 10) == 1) {
             printf("Erreur lors de l'initialisation de l'affichage");
+            free(chip8->RAM);
+            RAM_destroy(chip8->RAM);
             exit(1);
         }
 
         chip8->keyboard = malloc(sizeof(struct Keyboard));
         if (Keyboard_init(chip8->keyboard) == 1) {
             printf("Erreur lors de l'initialisation du clavier");
+            free(chip8->display);
+            RAM_destroy(chip8->RAM);
             exit(1);
         }
 
         chip8->speaker = malloc(sizeof(struct Speaker));
         if (Speaker_init(chip8->speaker) == 1) {
             printf("Erreur lors de l'initialisation du haut-parleur");
+            free(chip8->display);
+            free(chip8->keyboard);
+            RAM_destroy(chip8->RAM);
             exit(1);
         }
 
@@ -36,11 +43,16 @@ void chip8_init() {
                                      chip8->speaker);
         if (chip8->processor == NULL) {
             printf("Erreur lors de l'initialisation du processeur");
+            free(chip8->display);
+            free(chip8->keyboard);
+            free(chip8->speaker);
+            Proc_destroy(chip8->processor);
+            RAM_destroy(chip8->RAM);
             exit(1);
         }
 
 
-        chip8_load(chip8, "/home/romain/Documents/projet\ c/projetchipeight/rom/Airplane.ch8");
+        chip8_load(chip8, "/home/mlanglois/Documents/ProjetChipEight/rom/Airplane.ch8");
         chip8_cycle(chip8);
     }
     if (errcode != QUIT) {
@@ -56,6 +68,7 @@ void chip8_destroy(struct chip8* chip8) {
     Keyboard_destroy(chip8->keyboard);
     Speaker_destroy(chip8->speaker);
     free(chip8);
+    SDL_Quit();
 }
 
 void chip8_load(struct chip8* chip8, const char* path) {
@@ -83,8 +96,10 @@ void chip8_load(struct chip8* chip8, const char* path) {
 
 // Viser 500Hz
 void chip8_cycle(struct chip8* chip8) {
+    uint running = 1;
     int cpt = 0;
-    while (1) {
+    Display_CLS(chip8->display);
+    while (running) {
         processor_fetch_decode_execute(chip8->processor);
         if (cpt == 8) {
             chip8_refresh_screen(chip8);
