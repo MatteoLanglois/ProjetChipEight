48c69ab67cbfe7a372d8746e738a4373ca8b3c9f
matteolanglois <langlois.matteo49@gmail.com>
2024-01-19 11:12:51 +0100

    debug

--- chip8.c
+++ chip8.c
@@ -105,7 +105,7 @@ void chip8_cycle(struct chip8* chip8) {
             chip8_dec_timers(chip8);
             cpt = 0;
         }
-        SDL_Delay(2);
+        SDL_Delay(2000);
         cpt++;
     }
     chip8_destroy(chip8);
