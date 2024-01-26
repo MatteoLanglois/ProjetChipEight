5bcbbedb13cf837a519601fc5e495dbf27c07aed
matteolanglois <langlois.matteo49@gmail.com>
2024-01-26 11:16:15 +0100

    Upgrade main.c

--- main.c
+++ main.c
@@ -7,33 +7,32 @@
 int main() {
     struct dirent *dir;
     DIR *d = opendir("../rom");
-    int i = 1;
     char** roms_name = calloc(100,sizeof(char*));
-    if (d)
-    {
+    if (d) {
+        int index_boucle = 1;
+        int choix;
         printf("Liste des ROMs disponibles (avec le numéro correspondant):\n");
-        while ((dir = readdir(d)) != NULL)
-        {
-            if(strcmp(dir->d_name, "..") == 0 || strcmp(dir->d_name, ".") == 0)
-                continue;
-            else {
-                printf("%d -> %s\n", i, dir->d_name);
-                roms_name[i-1] = dir->d_name;
-                i++;
+        while ((dir = readdir(d)) != NULL) {
+            if(!(strcmp(dir->d_name, "..") == 0 || strcmp(dir->d_name, ".") == 0)) {
+                printf("%d -> %s\n", index_boucle, dir->d_name);
+                roms_name[index_boucle-1] = dir->d_name;
+                index_boucle++;
             }
         }
         closedir(d);
-    }
-    int choix;
-    do {
-        printf("Choisissez une ROM à lancer (avec le numéro correspondant) : ");
-        scanf("%d", &choix);
-    } while(choix < 1 || choix > i);
+        do {
+            printf("Choisissez une ROM à lancer (avec le numéro correspondant) : ");
+            scanf("%d", &choix);
+        } while(choix < 1 || choix > index_boucle);
 
-    char* test = roms_name[choix-1];
-    char* res = malloc(sizeof(char)*100);
-    sprintf(res, "../rom/%s", test);
+        char* choosed_rom = roms_name[choix-1];
+        char* res = malloc(sizeof(char)*100);
+        sprintf(res, "../rom/%s", choosed_rom);
+        chip8_init(res);
+        free(res);
+    } else {
+        printf("Le dossier n'est pas valide\n");
+    }
+
     free(roms_name);
-    chip8_init(res);
-    free(res);
 }
