f6de3875caa22aee6a5036eb4f3ab3ffe428c1e2
matteolanglois <langlois.matteo49@gmail.com>
2024-01-31 13:47:53 +0100

    Comment main.c

--- main.c
+++ main.c
@@ -20,20 +20,32 @@ int main() {
 }
 
 char* getChoosedRom() {
+    // On initialise le dossier
     struct dirent *dir;
+    // On choisit le dossier à ouvrir
     char* path = "../rom";
+    // On ouvre le dossier
     DIR *d = opendir(path);
+    // Si le dossier n'existe pas
     if (!d) {
+        // On ouvre un autre dossier
         path = "rom";
+        // On ouvre le dossier
         d = opendir(path);
+        // Si le dossier n'existe toujours pas
         if (!d) {
+            // On affiche un message d'erreur
             printf("Le dossier n'est pas valide\n");
         }
     }
 
+    // On initialise le tableau de noms de ROMs
     char** roms_name = calloc(100,sizeof(char*));
+    // On initialise l'index de boucle
     int index_boucle = 1;
+    // On initialise le choix de l'utilisateur
     int choix;
+    // On affiche les ROMs disponibles
     printf("Liste des ROMs disponibles (avec le numéro correspondant):\n");
     while ((dir = readdir(d)) != NULL) {
         if(!(strcmp(dir->d_name, "..") == 0 || strcmp(dir->d_name, ".") == 0)) {
@@ -42,15 +54,19 @@ char* getChoosedRom() {
             index_boucle++;
         }
     }
+    // On ferme le dossier
     closedir(d);
+    // On demande à l'utilisateur de choisir une ROM
     do {
         printf("Choisissez une ROM à lancer (avec le numéro correspondant) : ");
         scanf("%d", &choix);
+    // Tant que le choix n'est pas valide
     } while(choix < 1 || choix > index_boucle);
 
+    // On initialise le chemin de la ROM
     char* res = malloc(sizeof(char)*100);
     sprintf(res, "%s/%s", path, roms_name[choix-1]);
 
-
+    // On retourne le chemin de la ROM
     return res;
 }
